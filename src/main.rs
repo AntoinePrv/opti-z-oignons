7caeeae7e51e8f7ebfc83008fbc7c479a6a4fa15
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-03-06 14:44:23 +0100

    Add persons entry

--- src/main.rs
+++ src/main.rs
@@ -32,37 +32,71 @@ fn Home() -> Element {
 #[component]
 fn Schema() -> Element {
     let mut tables = use_signal(|| vec![6u32]);
-    const N_SEATS_ID: &'static str = "n_seats";
+    const TABLE_SEATS_ID: &'static str = "n_seats";
+
+    let mut persons = use_signal(Vec::<String>::new);
+    const PERSON_NAME_ID: &'static str = "name";
 
     rsx! {
+        p { "Tables:" }
         ul {
             for (i , seats) in tables.iter().enumerate() {
                 // TODO Missing key
                 li { "Table {i} ({seats} seats)" }
             }
         }
+
+        p { "Persons:" }
+        ul {
+            for person in persons.iter() {
+                // TODO Missing key
+                li { "{person}" }
+            }
+        }
+
         form {
             onsubmit: move |event| {
                 let n_seats_input = event
                     .data
                     .values()
-                    .remove(N_SEATS_ID)
+                    .remove(TABLE_SEATS_ID)
                     .map(|val| val.as_value())
                     .and_then(|val| val.parse::<u32>().ok());
                 if let Some(n_seats) = n_seats_input {
                     tables.push(n_seats)
                 }
             },
-            label { r#for: N_SEATS_ID, "Number of seats" }
+            label { r#for: TABLE_SEATS_ID, "Number of seats" }
             input {
-                id: N_SEATS_ID,
-                name: N_SEATS_ID,
+                id: TABLE_SEATS_ID,
+                name: TABLE_SEATS_ID,
                 r#type: "number",
                 min: 0,
                 step: 1,
                 value: 6,
             }
             button { r#type: "submit", "Add a table" }
+        }
+
+        form {
+            onsubmit: move |event| {
+                let name_input = event
+                    .data
+                    .values()
+                    .remove(PERSON_NAME_ID)
+                    .map(|val| val.as_value());
+                if let Some(name) = name_input {
+                    persons.push(name)
+                }
+            },
+            label { r#for: PERSON_NAME_ID, "Name" }
+            input {
+                id: PERSON_NAME_ID,
+                name: PERSON_NAME_ID,
+                r#type: "text",
+                minlength: 1,
+            }
+            button { r#type: "submit", "Add a person" }
         }
     }
 }
