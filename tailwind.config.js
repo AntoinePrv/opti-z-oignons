52269b16392131c5d277e52a936552516d398866
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-04-27 21:28:42 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "all",
+    content: [
+        "./assets/**/*.{html}",
+        "./src/**/*.{rs,html,css}",
+        "./dist/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
